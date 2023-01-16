442fdc21a84808bb7d1b60780fda77b5f76d96c6
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-01-16 11:35:37 +0200

    Bean testing (Application Context)

---
+++ TestSpring/src/main/java/ua/kucherenko/testspring/TestBean.java
@@ -0,0 +1,17 @@
+package ua.kucherenko.testspring;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ TestSpring/src/main/java/ua/kucherenko/testspring/TestSpring.java
@@ -0,0 +1,13 @@
+package ua.kucherenko.testspring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml");
+        TestBean bean = context.getBean("testBean", TestBean.class);
+        System.out.println(bean.getName());
+        context.close();
+    }
+}
