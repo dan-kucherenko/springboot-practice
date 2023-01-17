8e8f394f77fb9858ecfe3ac9665175dc4d976eb2
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-01-17 23:32:01 +0200

    DI using setter methods

--- TestSpring/src/main/java/ua/kucherenko/testspring/MusicPlayer.java
+++ TestSpring/src/main/java/ua/kucherenko/testspring/MusicPlayer.java
@@ -2,8 +2,30 @@ package ua.kucherenko.testspring;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
     // Inversion of control
     public MusicPlayer(Music music){
+        this.music = music;
+    }
+    public MusicPlayer(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music ){
         this.music = music;
     }
     public void playMusic(){
--- TestSpring/src/main/java/ua/kucherenko/testspring/TestSpring.java
+++ TestSpring/src/main/java/ua/kucherenko/testspring/TestSpring.java
@@ -9,8 +9,10 @@ public class TestSpring {
 //        Music music = context.getBean("musicBean", Music.class);
         // Manual dependency injection
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
         context.close();
     }
 }
