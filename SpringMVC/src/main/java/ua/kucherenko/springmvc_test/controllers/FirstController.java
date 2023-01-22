2e3652de9af9b3747cfd7aa5ec6be6da06cd880d
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-01-22 15:34:28 +0200

    GET method params

--- SpringMVC/src/main/java/ua/kucherenko/springmvc_test/controllers/FirstController.java
+++ SpringMVC/src/main/java/ua/kucherenko/springmvc_test/controllers/FirstController.java
@@ -2,16 +2,21 @@ package ua.kucherenko.springmvc_test.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String greetingPage(){
+    public String greetingPage(@RequestParam(value = "name", required = false) String name,
+                               @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello, " + name + ' ' + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage(){
+    public String goodbyePage() {
         return "first/goodbye";
     }
 }
