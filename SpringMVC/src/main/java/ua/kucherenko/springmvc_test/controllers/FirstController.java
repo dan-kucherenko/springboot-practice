920aebde99f50cba9aee766f0796f4f07369ccb3
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-01-22 16:29:05 +0200

    Thymeleaf template

--- SpringMVC/src/main/java/ua/kucherenko/springmvc_test/controllers/FirstController.java
+++ SpringMVC/src/main/java/ua/kucherenko/springmvc_test/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ua.kucherenko.springmvc_test.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
     @GetMapping("/hello")
     public String greetingPage(@RequestParam(value = "name", required = false) String name,
-                               @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello, " + name + ' ' + surname);
+                               @RequestParam(value = "surname", required = false) String surname,
+                               Model model) {
+        model.addAttribute("message", "Hello, " + name + ' ' + surname);
         return "first/hello";
     }
 
